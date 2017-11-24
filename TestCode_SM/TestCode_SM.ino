41855a655af174a5b36a35f83382306b9bbc154b
Seth G <gman10192@gmail.com>
2017-11-24 15:15:53 -0600

    Inital creation

---
+++ TestCode_SM/TestCode_SM.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		TestCode_SM.ino
+ Created:	11/24/2017 3:15:18 PM
+ Author:	PC0123
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
